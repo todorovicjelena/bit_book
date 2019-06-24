d69c93a854005745365d6fefc9e6003121be8134
BIT Student <classroom@bgit.rs>
2019-06-24 11:37:49 +0200

    frist render

---
+++ bit_book/src/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header } from '../app/Header';
+
+
+//if not login display login.page 
+//else 
+
+
+class App extends React.Component {
+  render() {
+
+
+    return (
+
+      <Header />
+
+    )
+  }
+}
+
+
+export default App;
