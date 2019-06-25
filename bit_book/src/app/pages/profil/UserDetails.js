30b08db0fe0bae9e662da9453edfac3e39660274
BIT Student <classroom@bgit.rs>
2019-06-25 12:36:19 +0200

    profile page almost done

--- bit_book/src/app/pages/profil/UserDetails.js
+++ bit_book/src/app/pages/profil/UserDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { UserService } from '../../../services/UserService';
 
 
 export class UserDetails extends React.Component {
@@ -6,20 +7,55 @@ export class UserDetails extends React.Component {
         super(props);
 
         this.state = {
-
+            user: []
 
 
         }
 
     }
 
+    componentDidMount() {
+        this.getProfileId()
+    }
 
+
+    getProfileId() {
+        const id = this.props.match.params.peopleId
+
+        UserService.fetchProfilePic(id)
+
+            .then(user => {
+                this.setState({
+                    user: user
+                }
+
+                )
+            })
+
+
+    }
 
     render() {
+        const user = this.state.user
+        console.log(user)
+        if (!user) {
+            return (
+                <p>Loading user</p>
+            )
+        }
 
         return (
+            <div>
 
-            <p>details page</p>
+                <div className="profile-page userDetails">
+                    <img className="circle responsive-img" src={user.avatarUrl} width="300x" height="300px" />
+                    <h4> {user.nameFirst} {user.nameLast}</h4>
+                    <div className="textProfile">
+                        {/* <p>{user.about.bio}</p> */}
+                    </div>
+                    <p className="postsNumb"> <span className="circle">C</span> <span>15 posts</span><span className="circle">C</span> <span>30 Comments</span></p>
+                </div >
+            </div>
 
         )
     }
