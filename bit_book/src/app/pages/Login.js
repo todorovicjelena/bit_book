c32a8ac5a4a571ad6cf9fcc72f1d0bcb380ec0f3
BIT Student <classroom@bgit.rs>
2019-06-25 17:50:27 +0200

    changing

--- bit_book/src/app/pages/Login.js
+++ bit_book/src/app/pages/Login.js
@@ -7,44 +7,76 @@ export class Login extends React.Component {
         super(props);
 
         this.state = {
-            users: [],
-
+            username: '',
+            email: '',
+            pass: ''
 
         }
+
+
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handleUsername.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
-    //     componentDidMount()
 
+    handleSubmit() {
+        const username = this.state.username
+        const email = this.state.email
+        const pass = this.state.pass
 
-    // }
+        this.setState({
+            username: '',
+            email: '',
+            pass: ''
+        })
+
+    }
+
+    handleUsername(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleEmail(event) {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePassword(event) {
+        this.setState({
+            pass: event.target.value
+        })
+    }
+
 
     render() {
 
-        return (
-            <>
 
+        return (
+
+            <>
                 <p>BitBooK Login</p>
                 <div className="loginPage" >
-
-
-
                     <div className="buttons">
                         <input className="loginButton" type="button" value="Login"></input>
                         <input className="registerButton" type="button" value="Register"></input>
                     </div>
                     <div className="bodyLogin"> <span>email </span>
-                        <input type="" placeholder="Email Address"></input>
+                        <input type="" placeholder="Email Address" value={this.state.email} onChange={this.handleEmail}></input>
                         <p className="error">Error</p>
                         <span>pass </span>
-                        <input type="" placeholder="Password"></input>
+                        <input type="" placeholder="Password" value={this.state.pass} onChange={this.handlePassword}></input>
                     </div>
-                    <input className="loginToNextPage" type="button" value="Login"></input>
+                    <input className="loginToNextPage" type="button" value="Submit"></input>
                     <p className="error">Unable to login, invalid credentials</p>
-
                 </div>
-
-
             </>
+
         )
     }
-
 }
+
