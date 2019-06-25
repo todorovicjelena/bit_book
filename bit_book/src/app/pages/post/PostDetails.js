a9ad5c71cf0688b356377e0ce31f6baeb1cdd615
BIT Student <classroom@bgit.rs>
2019-06-25 11:39:36 +0200

    some single post element done

--- bit_book/src/app/pages/post/PostDetails.js
+++ bit_book/src/app/pages/post/PostDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { postservices } from '../../../services/PostService'
 
 
 export class PostDetails extends React.Component {
@@ -6,8 +7,22 @@ export class PostDetails extends React.Component {
         super(props)
         this.state = {}
     }
+    componentDidMount() {
+        this.getsingleUser()
+    }
+    getsingleUser() {
+        const id = this.props.match.params.postId
+        postservices.FetchSinglePosts(id)
+            .then(post => {
+                console.log(post);
+
+            })
+
+
+    }
 
     render() {
         return <p>this is postdetail page</p>
+
     }
 }
