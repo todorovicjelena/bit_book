d834100b8e8bdb8301af6a5117619b3179f92923
BIT Student <classroom@bgit.rs>
2019-06-25 09:58:17 +0200

    users details part1

---
+++ bit_book/src/app/pages/profil/UserDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export class UserDetails extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+
+        }
+
+    }
+    render() {
+
+        return (
+
+            <h1>Details Page</h1>
+        )
+    }
+
+}
