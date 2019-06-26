3965a9bc9d85cfbf225cfcd0ca019a30a9341b7f
BIT Student <classroom@bgit.rs>
2019-06-26 15:21:15 +0200

    login request

--- bit_book/src/services/RegisterService.js
+++ bit_book/src/services/RegisterService.js
@@ -1,4 +1,3 @@
-import React from 'react'
 
 
 class RegisterService {
@@ -28,6 +27,38 @@ class RegisterService {
                 console.log(myStorage);
 
             })
+
+    }
+
+    fetchLogin = (email, password) => {
+
+        const body = {
+            email: email,
+            password: password
+        }
+        console.log(body);
+
+        const myStorage = window.localStorage;
+
+        const login = 'https://book-api.hypetech.xyz/v1/auth/login'
+        return fetch(login, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'B1tD3V'
+            }
+        })
+
+            .then(response => response.json())
+            .then(res => {
+                myStorage.setItem('postToken', res.accessToke)
+
+            })
+
+
+
+
 
     }
 
